684d315aa3d0b6860ffe9c3dcfebf2d1fb744631
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-12-09 15:41:53 -0500

    removed buttons while i build out pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import styled from 'styled-components';
+import {gsap, TweenMax, Power3} from 'gsap';
+
+gsap.registerPlugin(TweenMax, Power3);
 
 const Container = styled.div`
 	max-width: 500px;
-	height: 90vh;
+	height: 100vh;
 	width: 90%;
 	margin: 0 auto;
 	display: flex;
@@ -15,6 +18,7 @@ const Container = styled.div`
 		margin-bottom: 8px;
 		font-size: 2rem;
 		font-weight: 700;
+		opacity: 0;
 	}
 
 	p {
@@ -22,6 +26,7 @@ const Container = styled.div`
 		font-size: 1rem;
 		font-weight: 500;
 		text-align: center;
+		opacity: 0;
 	}
 
 	a {
@@ -33,6 +38,7 @@ const Container = styled.div`
 		margin-bottom: 8px;
 		display: flex;
 		justify-content: space-evenly;
+		opacity: 0;
 
 		button {
 			margin: 0 0.25rem;
@@ -54,6 +60,7 @@ const Container = styled.div`
 	}
 
 	.icons {
+		opacity: 0;
 		a {
 			i {
 				margin: 0 0.25rem;
@@ -70,17 +77,53 @@ const Container = styled.div`
 `  
 
 const App = () => {
+	let name = useRef(null);
+    let bio = useRef(null);
+    let buttons = useRef(null);
+    let icons = useRef(null);
+    
+    useEffect(() => {
+        TweenMax.to(name, 1, {
+                opacity: 1,
+                y: -50,
+                ease: Power3.easeOut,
+                delay: 0.1
+            }
+        );
+        TweenMax.to(bio, 1, {
+                opacity: 1,
+                y: -50,
+                ease: Power3.easeOut,
+                delay: 0.2
+            }
+        );
+        // TweenMax.to(buttons, 1, {
+        //         opacity: 1,
+        //         y: -50,
+        //         ease: Power3.easeOut,
+        //         delay: 0.3
+        //     }
+        // );
+        TweenMax.to(icons, 1, {
+                opacity: 1,
+                y: -50,
+                ease: Power3.easeOut,
+                delay: 0.4
+            }
+        );
+    }, [])
+	
 	return (
 		<Container>
-			<h1>miguel nicolas</h1>
-			<p>Hi! I'm a front end software engineer based in New Jersey. Currently attending Lambda School.</p>
-			<div className='buttons'>
+			<h1 ref={element => {name = element}}>miguel nicolas</h1>
+			<p ref={element => {bio = element}}>Hi! I'm a front end software engineer based in New Jersey. Currently attending Lambda School.</p>
+			{/* <div className='buttons' ref={element => {buttons = element}}>
 				<button>Skills</button>
 				<button>Projects</button>
 				<button>Resume</button>
 				<button>Blog</button>
-			</div>
-			<div className='icons'>
+			</div> */}
+			<div className='icons' ref={element => {icons = element}}>
 				<a href='https://github.com/miugel' target='_blank' rel="noopener noreferrer"><i className="fab fa-github-square"></i></a>
 				<a href='https://www.linkedin.com/in/miguelqnicolas/' target='_blank' rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
 				<a href='mailto:miguelqnicolas@gmail.com' target='_blank' rel="noopener noreferrer"><i className="fas fa-envelope-square"></i></a>
