835e20dd9dfcea0af859ea8ca5969f4832877c74
Miguel <miguelqnicolas@gmail.com>
2020-04-06 01:38:06 -0400

    started on the skills component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
-import Home from './components/Home';
-import Projects from './components/Projects';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/home';
+import Projects from './components/projects';
+import Skills from './components/skills';
+import PageNotFound from './components/pageNotFound';
 
 const App = () => {
 	return (
-		<>
-			<Route exact path='/' component={Home}/>
-			<Route path='/projects' component={Projects}/>
-		</>
+		<Switch>
+			<Route exact path='/' component={Home} />
+			<Route path='/projects' component={Projects} />
+			<Route path='/skills' component={Skills} />
+			<Route component={PageNotFound} />
+		</Switch>
 	);
 };
 
