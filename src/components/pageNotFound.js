9889527af87f007ab5948aa85a981240cc35c43a
Miguel Nicolas <miguelqnicolas@gmail.com>
2021-04-11 00:21:08 -0400

    Update PageNotFound component

--- src/components/pageNotFound.js
+++ src/components/pageNotFound.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Page } from './common';
-import { PageNotFoundContainer } from './styles';
+import styled from 'styled-components';
+import { Button } from './common';
+
+export const PageNotFoundContainer = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    height: 90vh;
+    justify-content: center;
+`;
 
 export const PageNotFound = () => (
-    <Page>
-        <PageNotFoundContainer>
-            <h1>Page not found</h1>
-            <Link to='/'><button>Go back</button></Link>
-        </PageNotFoundContainer>
-    </Page>
+    <PageNotFoundContainer>
+        <h1>Page not found</h1>
+        <Link to='/'><Button text='Go back' /></Link>
+    </PageNotFoundContainer>
 );
--- src/components/styles/index.js
+++ src/components/styles/index.js
@@ -1,4 +1,3 @@
 export { HomeContainer } from './homeStyle';
-export { PageNotFoundContainer } from './pageNotFoundStyle';
 export { ProjectsContainer } from './projectsStyle';
 export { SkillsContainer } from './skillsStyle';
